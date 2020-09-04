54107798a5b56a01975adf7840be2127a8fe6d26
Tigran Avushyan <54512704+TigranAvushyan@users.noreply.github.com>
2020-09-04 10:32:30 +0300

    Add files via upload

--- Chart.js
+++ Chart.js
@@ -1,112 +1,229 @@
 class Chart {
-    constructor(canvas_id, param, option = {}) {
+    constructor(canvas_id, data, option) {
         this.canvas = document.getElementById(canvas_id)
         this.ctx = this.canvas.getContext("2d")
         this.canvas.width = this.canvas.offsetWidth
         this.canvas.height = this.canvas.offsetHeight
-        this.param = param
-
-        const defaultOption = {
-            padding: {
-                left: 10,
-                right: 10,
-                top: 10,
-                bottom: 10
-            },
-            axis: {
-                lineWidth: {
-                    X: 1,
-                    Y: 1
-                },
-                color : {
-                    X: 3,
-                    Y: 2
-                }
-            }
-        }
-
-        this.option = Object.assign(defaultOption, option)
-    }
-
-
-    // Chart body size
-    chartBodySize() {
-        return {
+        this.data = data
+        this.option = option
+        this.chartBodySize = { 
             WIDTH: this.canvas.width - this.option.padding.left - this.option.padding.right,
             HEIGHT: this.canvas.height - this.option.padding.top - this.option.padding.bottom,
         }
-    }
-
-
-    axisZone() {
-        return {
-            start_X: this.chartBodySize().HEIGHT + this.option.padding.top,
-            start_Y: this.option.padding.left,
-            end_X: this.option.padding.top,
-            end_Y: this.chartBodySize().WIDTH + this.option.padding.left,
-        }
-    }
-
-    axisUnit() {
-        return {
-            X: quantity => {
-                const arr = []
-                const count = this.chartBodySize().HEIGHT / quantity
-                for (let i = this.axisZone().start_X; i >= this.axisZone().end_X; i -= count) {
-                    arr.push(i)
-                }
-                return arr
+        
+        this.chartZone = {
+            left_top: {
+                X: this.option.padding.left,
+                Y: this.option.padding.top
             },
-            Y: quantity => {
-                const arr = []
-                const count = this.chartBodySize().WIDTH / quantity
-                for (let i = this.axisZone().start_Y; i <= this.axisZone().end_Y; i += count) {
-                    arr.push(i)
+            left_bottom: {
+                X: this.option.padding.left,
+                Y: this.chartBodySize.HEIGHT + this.option.padding.top
+            },
+            right_top: {
+                X: this.chartBodySize.WIDTH + this.option.padding.left,
+                Y: this.option.padding.top
+            },
+            right_bottom: {
+                X: this.chartBodySize.WIDTH + this.option.padding.left,
+                Y: this.chartBodySize.HEIGHT + this.option.padding.top
+            }
+        }
+        this.quantity = {
+            X: 4,
+            Y: this.data.reduce((maxLength, item) => {
+                return maxLength.length < item.length ? item : maxLength
+            }).length
+        }
+        this.offset = []
+        this.option.dataStyle.forEach(item => {
+            this.offset.push(item.animation.speed)
+        })
+        console.log(this.offset);
+    }
+    
+    
+    
+    axisGridUnit() {
+        const arrX = []
+            const arrY = []
+            const countY = (this.chartBodySize.HEIGHT + this.option.axis.X.grid.lineWidth) / this.quantity.X
+            const countX = (this.chartBodySize.WIDTH + this.option.axis.Y.grid.lineWidth) / this.quantity.Y
+            
+            for (let i = this.chartZone.left_bottom.X; i <= this.chartZone.right_bottom.X; i += countX)
+            arrX.push(i)
+            for (let i = this.chartZone.left_bottom.Y; i >= this.chartZone.left_top.Y; i -= countY)
+            arrY.push(i)
+            return { X: arrX, Y: arrY }
+        }
+        
+        
+        
+        // Draw
+        
+    drawChart() {
+        this.ctx.lineCap = this.ctx.lineJoin = 'round'
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        if(this.option.axis.X.grid.visible)
+            this.axisGridDraw().X()
+        if(this.option.axis.Y.grid.visible)
+            this.axisGridDraw().Y()
+        if(this.option.axis.X.main.visible)
+            this.axisMainDraw().X()
+        if(this.option.axis.Y.main.visible)
+            this.axisMainDraw().Y()
+
+
+        const dataCircleAndAnimation = this.dataUnit()
+        dataCircleAndAnimation.forEach((item, ind) => {
+            
+            if (this.option.dataStyle[ind].point != undefined){
+                item.forEach((data, index) => {
+                    if (index != 0) {
+                        this.ctx.beginPath()
+                        let radius = this.option.dataStyle[ind].point.radius
+                        this.ctx.fillStyle = this.option.dataStyle[ind].point.color
+                        this.ctx.arc(data[0], data[1], radius, 0, 2 * Math.PI, false)
+                        this.ctx.fill()
+                    }
+                })
+            }
+
+            setTimeout(() => {this.drawChart()}, 20)
+        })
+        
+    }
+    
+    dataUnit() {
+        let maxX,maxY,minX,minY
+        this.data.forEach(item => {
+            item.forEach(data => {
+                if(data[0] >= (maxX || data[0])) {maxX = data[0]}
+                if(data[1] >= (maxY || data[1])) {maxY = data[1]}
+                if(data[0] <= (minX || data[0])) {minX = data[0]}
+                if(data[1] <= (minY || data[1])) {minY = data[1]}
+            })
+        })
+        const arr = []
+        this.data.forEach((item, ind) => {
+            const dataArr = []
+            item.sort((a, b) => a[0] - b[0])
+            this.ctx.beginPath()
+            this.ctx.lineWidth = this.option.dataStyle[ind].lineWindt
+            this.ctx.strokeStyle = this.option.dataStyle[ind].color
+            this.ctx.setLineDash(this.option.dataStyle[ind].lineDash)
+
+            if(this.option.dataStyle[ind].animation != undefined) {
+                this.ctx.lineDashOffset = this.offset[ind] -= this.option.dataStyle[ind].animationSpeed
+            }
+            item.forEach(data => {
+                let x = data[0] * (this.chartBodySize.WIDTH) / (maxX + minX)
+                let y = this.chartBodySize.HEIGHT + this.option.padding.top - data[1] * (this.chartBodySize.HEIGHT) / (maxY + minY)
+                if (data[0] == minX) {
+                    this.ctx.moveTo(this.chartZone.left_bottom.X, y)
+                    dataArr.push([this.chartZone.left_bottom.X, y])
                 }
-                return arr
+                else {
+                    this.ctx.lineTo(x, y)
+                    dataArr.push([x, y])
+                }
+                this.ctx.stroke()
+            })
+            arr.push(dataArr)
+        })
+        return arr
+    }
+
+    axisGridDraw() {
+        return {
+            Y: () => {
+                this.ctx.lineWidth = this.option.axis.Y.grid.lineWidth
+                this.ctx.strokeStyle = this.option.axis.Y.grid.color
+                this.ctx.setLineDash(this.option.axis.Y.grid.lineDash)
+                for (let i = 1; i < this.axisGridUnit().Y.length; i++) {
+                    this.ctx.beginPath()
+                    this.ctx.moveTo(this.chartZone.left_bottom.X, this.axisGridUnit().Y[i])
+                    this.ctx.lineTo(this.chartZone.right_bottom.X - 15, this.axisGridUnit().Y[i])
+                    this.ctx.stroke()
+                }
+            },
+            X: () => {
+                this.ctx.lineWidth = this.option.axis.X.grid.lineWidth
+                this.ctx.strokeStyle = this.option.axis.X.grid.color
+                this.ctx.setLineDash(this.option.axis.X.grid.lineDash)
+                for (let i = 1; i < this.axisGridUnit().X.length; i++) {
+                    this.ctx.beginPath()
+                    this.ctx.moveTo(this.axisGridUnit().X[i], this.chartZone.left_top.Y + 15)
+                    this.ctx.lineTo(this.axisGridUnit().X[i], this.chartZone.left_bottom.Y)
+                    this.ctx.stroke()
+                }
             }
         }
     }
-
-
-    // Draw
-    drawChartBody() {
-        this.ctx.rect(this.option.padding.left,
-            this.option.padding.top,
-            this.chartBodySize().WIDTH,
-            this.chartBodySize().HEIGHT)
-        this.ctx.fill()
-    }
-
-    drawChart() {
-        this.chartAxisDraw().X()
-        this.chartAxisDraw().Y()
-    }
-
-
-    chartAxisDraw() {
+    
+    axisMainDraw() {
         return {
-            X: () => {
-                // this.ctx.lineWidth = this.option.axis.lineWidth.X
-                this.ctx.strokeStyle = this.option.axis.color.X
-                this.axisUnit().X(10).forEach(item => {
+            X : () => {
+                this.ctx.lineWidth = this.option.axis.X.main.lineWidth
+                this.ctx.strokeStyle = this.option.axis.X.main.color
+                this.ctx.setLineDash(this.option.axis.X.main.lineDash)
+                this.ctx.beginPath()
+                this.ctx.moveTo(this.chartZone.left_bottom.X, this.chartZone.left_bottom.Y)
+                this.ctx.lineTo(this.chartZone.right_bottom.X, this.chartZone.right_bottom.Y)
+                this.ctx.stroke()
+
+                if (this.option.axis.X.main.arrow.visible){
+                    const x1 = this.chartZone.right_bottom.X
+                    const y1 = this.chartZone.right_bottom.Y
+                    const width = this.option.axis.X.main.arrow.width
+                    const angle = this.option.axis.X.main.arrow.angle
                     this.ctx.beginPath()
-                    this.ctx.moveTo(this.axisZone().start_Y, item)
-                    this.ctx.lineTo(this.axisZone().end_Y, item)
+                    this.ctx.moveTo(x1, y1)
+                    this.ctx.lineTo(x1 - width * Math.sin(angle), y1 + width * Math.cos(angle))
+                    this.ctx.strokeStyle = this.option.axis.X.main.arrow.stroke.color
+                
+                    if(this.option.axis.Y.main.arrow.fill.visible){
+                        this.ctx.lineTo(x1 + width * Math.sin(angle), y1 - width * Math.cos(angle))
+                        this.ctx.closePath()
+                        this.ctx.fillStyle = this.option.axis.Y.main.arrow.fill.color
+                        this.ctx.fill()
+                    } else { 
+                        this.ctx.moveTo(x1, y1)
+                        this.ctx.lineTo(x1 - width * Math.sin(angle), y1 - width * Math.cos(angle))
+                    }
                     this.ctx.stroke()
-                    this.ctx.closePath()
-                })
+                }
+
             },
-            Y: () => {
-                // this.ctx.lineWidth = this.option.axis.lineWidth.Y
-                this.ctx.strokeStyle = this.option.axis.color.Y
-                this.axisUnit().Y(4).forEach(item => {
+            Y : () => {
+                this.ctx.lineWidth = this.option.axis.Y.main.lineWidth
+                this.ctx.strokeStyle = this.option.axis.Y.main.color
+                this.ctx.setLineDash(this.option.axis.Y.main.lineDash)
+                this.ctx.beginPath()
+                this.ctx.moveTo(this.chartZone.left_bottom.X, this.chartZone.left_bottom.Y)
+                this.ctx.lineTo(this.chartZone.left_top.X, this.chartZone.left_top.Y)
+                this.ctx.stroke()
+
+                if (this.option.axis.Y.main.arrow.visible){
+                    const x1 = this.chartZone.left_top.X
+                    const y1 = this.chartZone.left_top.Y
+                    const width = this.option.axis.Y.main.arrow.width
+                    const angle = this.option.axis.Y.main.arrow.angle
                     this.ctx.beginPath()
-                    this.ctx.moveTo(item, this.axisZone().end_X)
-                    this.ctx.lineTo(item, this.axisZone().start_X)
+                    this.ctx.moveTo(x1, y1)
+                    this.ctx.lineTo(x1 + width * Math.cos(angle), y1 + width * Math.sin(angle))
+                    this.ctx.strokeStyle = this.option.axis.Y.main.arrow.stroke.color
+                    if(this.option.axis.Y.main.arrow.fill.visible){
+                        this.ctx.lineTo(x1 - width * Math.cos(angle), y1 + width * Math.sin(angle))
+                        this.ctx.closePath()
+                        this.ctx.fillStyle = this.option.axis.Y.main.arrow.fill.color
+                        this.ctx.fill()
+                    } else { 
+                        this.ctx.moveTo(x1, y1)
+                        this.ctx.lineTo(x1 - width * Math.cos(angle), y1 + width * Math.sin(angle))
+                    }
                     this.ctx.stroke()
-                    this.ctx.closePath()
-                })
+                }
             }
         }
     }
--- index.js
+++ index.js
@@ -1,19 +1,119 @@
 function createChart() {
-    const chart = new Chart("chart", 2,{
+    const chart = new Chart("chart", [
+        [
+            [1,2],[4,10],[5,6],[7,1],[8,3]
+        ],
+        [
+            [5,2],[1,10],[7,6],[6,1],[10,3]
+        ]
+    ],{
         padding: {
-            left: 100,
-            right: 50,
+            left: 5,
+            right: 5,
+            bottom: 5,
+            top: 5
         },
+        dataStyle: [
+            {
+                color: "red",
+                lineWindt: 3,
+                lineDash: [5, 6],
+                animation: {
+                    speed: 2
+                },
+                point: {
+                    radius: 8,
+                    color: "yellow"
+                }
+            },
+            {
+                color: "blue",
+                lineWindt: 1,
+                lineDash: [2,8],
+                animation: {
+                    speed: 2
+                }
+            },
+        ],
         axis: {
-            lineWidth: {
-                X: 1,
-                Y: 1
+            X: {
+                grid: {
+                    lineDash: [],
+                    visible: true,
+                    color: "black",
+                    lineWidth: 1,
+                    name: {
+                        visible: true,
+                        fontSize: 16,
+                        color: "black",
+                        fontFamily: "Roboto"
+                    }
+                },
+                main: {
+                    arrow: {
+                        width: 5,
+                        visible: true,
+                        angle: 45,
+                        stroke: {
+                            color: "black"
+                        },
+                        fill: {
+                            visible: false,
+                            color: "black"
+                        }
+                    },
+                    lineDash: [],
+                    visible: true,
+                    color: "black",
+                    lineWidth: 1,
+                    name: {
+                        visible: true,
+                        fontSize: 16,
+                        color: "black",
+                        fontFamily: "Roboto"
+                    }
+                },
+            },
+            Y: {
+                grid: {
+                    lineDash: [],
+                    visible: true,
+                    color: "black",
+                    lineWidth: 1,
+                    name: {
+                        visible: true,
+                        fontSize: 16,
+                        color: "black",
+                        fontFamily: "Roboto"
+                    }
+                },
+                main: {
+                    arrow: {
+                        width: 5,
+                        visible: true,
+                        angle: 45,
+                        stroke: {
+                            color: "black"
+                        },
+                        fill: {
+                            visible: false,
+                            color: "black"
+                        }
+                    },
+                    lineDash: [],
+                    visible: true,
+                    color: "black",
+                    lineWidth: 1,
+                    name: {
+                        visible: true,
+                        fontSize: 16,
+                        color: "black",
+                        fontFamily: "Roboto"
+                    }
+                },
             },
         }
     })
-
-    console.log(chart.option)
+    chart.drawChart()
 }
-
-window.addEventListener("resize", createChart)
-window.addEventListener("load", createChart)
+createChart()
